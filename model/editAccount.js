f4d6727c9d9b29d0305d154cc672f90d8ab4b517
Fahrizal Septrianto <fahrizalseptrianto@gmail.com>
2016-03-01 23:43:59 +0700

    ver 1.6.4 bug editAccount fixed

--- model/editAccount.js
+++ model/editAccount.js
@@ -14,105 +14,86 @@ editAccount.prototype.handleRoutes = function(router,connection,md5){
     var passwordType = md5(req.body.passwordAdminType);
     var newPassword = md5(req.body.newPassword);
     var confirmation = md5(req.body.confirmation);
-    if(session==null || session==undefined || session==""){
-      res.json({"message":"err.. no params session rec"});
+    if(usernameType==null || usernameType==undefined || usernameType==""){
+      res.json({"message":"err.. no params usernameType"});
     }else{
-        if(usernameType==null || usernameType==undefined || usernameType==""){
-          res.json({"message":"err.. no params usernameType rec"});
+      if(req.body.passwordAdminType==null || req.body.passwordAdminType==undefined || req.body.passwordAdminType==""){
+        res.json({"message":"err.. no params passwordType"});
+      }else{
+        if(session==null || session==undefined || session==""){
+          res.json({"message":"err.. no params rec"});
         }else{
-          if(passwordType==null || passwordType==undefined || passwordType==""){
-            res.json({"message":"err.. no params passwordType rec"});
-          }else{
-            var query="select id_user from `session` where session_code='"+session+"'";
-            connection.query(query,function(err,rows){
-              if(err){
-                res.json({"message":"err.. error in selectiong session","query":query});
-              }else{
-                if(rows.length>0){
-                  var idUserz = rows[0].id_user;
-                  connection.query("select username,password,id_role from `user` where id_user="+idUserz,function(err,rows){
-                    if(err){
-                      res.json({"message":"err.. error on selecting user"});
-                    }else{
-                      if(rows.length>0){
-                        var usernameLocal = rows[0].username;
-                        var passwordLocal = rows[0].password;
-                        var idRole = rows[0].id_role;
-                        connection.query("select role_name from `role` where id_role="+idRole,function(err,rows){
-                          if(err){
-                            res.json({"messgae":"err.. error on selecting role"});
-                          }else{
-                            if(rows.length>0){
-                              if(rows[0].role_name == 'admin'){
-                                if(passwordType == passwordLocal){
-                                  //checking local username with local password to database
-                                  connection.query("select id_user,password from `user` where username='"+usernameLocal+"'",function(err,rows){
-                                    if(err){
-                                      res.json({"message":"err.. error on checking user lcl"});
+          connection.query("select id_user from `session` where session_code='"+session+"'",function(err,rows){
+            if(err){
+              res.json({"message":"err.. error on selecting session"});
+            }else{
+              if(rows.length>0){
+                var idUser = rows[0].id_user;
+                connection.query("select id_role,username,password from `user` where id_user="+idUser,function(err,rows){
+                  if(err){
+                    res.json({"message":"err.. error on selecting user"});
+                  }else{
+                    if(rows.length>0){
+                      var idRole = rows[0].id_role;
+                      var password = rows[0].password;
+                      connection.query("select role_name from `role` where id_role="+idRole,function(err,rows){
+                        if(err){
+                          res.json({"message":"err.. error on selecting role"});
+                        }else{
+                          if(rows.length>0){
+                            var roleName = rows[0].role_name;
+                            if(roleName == 'admin'){
+                              if(password == passwordType){
+                                connection.query("update `user` set username='"+usernameType+"' where id_user="+idUser,function(err,rows){
+                                  if(err){
+                                    res.json({"message":"err.. error on updating username"});
+                                  }else{
+                                    //success updating username
+                                    if(req.body.newPassword==null || req.body.newPassword==undefined || req.body.newPassword=="" || req.body.newPassword=='' || req.body.newPassword==" "){
+                                      res.json({"message":"Success updating your username","error":"success"});
                                     }else{
-                                      if(rows.length>0){
-                                        if(rows[0].password == passwordLocal){
-                                          if(passwordLocal == passwordType){
-                                            var idUser = rows[0].id_user;
-                                            if(newPassword == null || newPassword==undefined || newPassword==""){
-                                              connection.query("update `user` set username='"+usernameType+"' where id_user="+idUser,function(err,rows){
-                                                if(err){
-                                                  res.json({"message":"err.. error on updating username"});
-                                                }else{
-                                                  res.json({"message":"success on updating username"});
-                                                }
-                                              });
-                                            }else{
-                                              if(confirmation==null || confirmation==undefined || confirmation==""){
-                                                res.json({"message":"err.. error, you fill your newPassword but didn't fill the confirmation password"});
-                                              }else{
-                                                if(newPassword == confirmation){
-                                                  connection.query("update `user` set username='"+usernameType+"',password='"+newPassword+"' where id_user="+idUser,function(err,rows){
-                                                    if(err){
-                                                      res.json({"message":"err.. error on updating username & password"});
-                                                    }else{
-                                                      res.json({"message":"success on updating username & password"});
-                                                    }
-                                                  });
-                                                }else{
-                                                  res.json({"message":"err.. error, your new password and confirmation didn't match"});
-                                                }
-                                              }
-                                            }
-                                          }else{
-                                            res.json({"message":"err.. your password didn't match with your data saved"});
-                                          }
-                                        }else{
-                                          res.json({"message":"err.. passwordLocal didn't match your username"});
-                                        }
+                                      if(req.body.confirmation==null || req.body.confirmation==undefined || req.body.confirmation==""){
+                                        res.json({"message":"err.. error updating password, confirmation blank","error":"error"});
                                       }else{
-                                        res.json({"message":"err.. no rows on user with given lcl"});
+                                        if(newPassword == confirmation){
+                                          connection.query("update `user` set password='"+newPassword+"' where id_user="+idUser,function(err,rows){
+                                            if(err){
+                                              res.json({"message":"err.. error on updating  user password, success updating username","error":"equal"});
+                                            }else{
+                                              res.json({"message":"success updating your username and password","error":"success","newPassword":newPassword});
+                                            }
+                                          });
+                                          //res.json({"asdasd":"critical section","newPass":newPassword});
+                                        }else{
+                                          res.json({"message":"err.. error updating password, password confirmation wrong","error":"error"});
+                                        }
                                       }
                                     }
-                                  });
-                                }else{
-                                  res.json({"message":"err.. wrong typing your old password"});
-                                }
+                                  }
+                                });
                               }else{
-                                res.json({"message":"err.. not authorized"});
+                                res.json({"message":"err... error password mismatch","password":password,"passwordType":passwordType});
                               }
-                            }else {
-                              res.json({"message":"err.. no rows on role"});
+                            }else{
+                              res.json({"message":"err.. do not authorize"});
                             }
+                          }else{
+                            res.json({"message":"err.. error no role registered"});
                           }
-                        });
-                      }else{
-                        res.json({"message":"Err.. no rows on user"});
-                      }
+                        }
+                      });
+                    }else{
+                      res.json({"message":"err.. error no user registered"});
                     }
-                  });
-                }else{
-                  res.json({"message":"err.. no session registeered"});
-                }
+                  }
+                });
+              }else{
+                res.json({"message":"err.. error no session registered"});
               }
-            });
-          }
+            }
+          });
         }
+      }
     }
   });
 }
