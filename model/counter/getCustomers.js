58bc7bce3658b9b53c936ac44c9d19045956c5e0
Fahrizal Septrianto <fahrizalseptrianto@gmail.com>
2016-04-05 19:52:10 +0700

    ver 1.10.4 fix bugs getCustomers

--- model/counter/getCustomers.js
+++ model/counter/getCustomers.js
@@ -8,7 +8,7 @@ function getCustomers(router,connection){
 var self=this;
 
 getCustomers.prototype.handleRoutes = function(router,connection){
-  router.post('/getCustomers',function(err,rows){
+  router.post('/getCustomers',function(req,res){
     connection.query("select id,name,phone,email,membership_id,birthdate from `customer` ",function(err,rows){
       if(err){
         res.json({"message":"err.. error on selecting"});
