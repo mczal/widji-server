fdba6ac16776674df50f509a4ea04d17e628e20d
Fahrizal Septrianto <fahrizalseptrianto@gmail.com>
2016-05-02 00:12:39 +0700

    ver 2.7 deadlinemonitor : order,filtering

--- model/monitor/deadlineMonitor.js
+++ model/monitor/deadlineMonitor.js
@@ -7,14 +7,15 @@ function deadlineMonitor(router,connection){
 
 deadlineMonitor.prototype.handleRoutes = function(router,connection){
   router.post('/deadlineMonitor',function(req,res){
-    connection.query("select no_bon,name,jumlah_bayar,customer_id,created_at,tanggal_pengambilan,jam_pengambilan,datediff(tanggal_pengambilan,curDate()) as selisih,keterangan,status_pengerjaan,worker,laci from `order`",function(err,rows){
+    var query = "select no_bon,name,jumlah_bayar,customer_id,created_at,tanggal_pengambilan,jam_pengambilan,datediff(tanggal_pengambilan,curDate()) as selisih,keterangan,status_pengerjaan,worker,laci from `order`  where (datediff(tanggal_pengambilan,curDate())) >= -10 AND (datediff(tanggal_pengambilan,curDate())) <=14 order by selisih";
+    connection.query(query,function(err,rows){
       if(err){
-        res.json({"message":"err.. error on selecting name and jumByr"});
+        res.json({"message":"err.. error on selecting name and jumByr","quer":query});
       }else{
         if(rows.length>0){
           res.json({"message":"success selecting","error":"success","content":rows});
         }else{
-          res.json({"message":"err.. no rows"});
+          res.json({"message":".. no deadline"});
         }
       }
     });
