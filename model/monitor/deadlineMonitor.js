a0ef46fc3afe244bfbc4ffe34fbb706b899abeff
Fahrizal Septrianto <fahrizalseptrianto@gmail.com>
2016-05-02 00:26:25 +0700

    ver 2.7.1 getOrderItem: order by category ; deadlineMonitor: debug curDate()

--- model/monitor/deadlineMonitor.js
+++ model/monitor/deadlineMonitor.js
@@ -7,7 +7,7 @@ function deadlineMonitor(router,connection){
 
 deadlineMonitor.prototype.handleRoutes = function(router,connection){
   router.post('/deadlineMonitor',function(req,res){
-    var query = "select no_bon,name,jumlah_bayar,customer_id,created_at,tanggal_pengambilan,jam_pengambilan,datediff(tanggal_pengambilan,curDate()) as selisih,keterangan,status_pengerjaan,worker,laci from `order`  where (datediff(tanggal_pengambilan,curDate())) >= -10 AND (datediff(tanggal_pengambilan,curDate())) <=14 order by selisih";
+    var query = "select no_bon,name,jumlah_bayar,customer_id,created_at,tanggal_pengambilan,jam_pengambilan,datediff(tanggal_pengambilan,curDate()) as selisih,keterangan,status_pengerjaan,worker,laci,curDate() as debug_curTime from `order`  where (datediff(tanggal_pengambilan,curDate())) >= -10 AND (datediff(tanggal_pengambilan,curDate())) <=14 order by selisih";
     connection.query(query,function(err,rows){
       if(err){
         res.json({"message":"err.. error on selecting name and jumByr","quer":query});
--- model/monitor/getOrderItem.js
+++ model/monitor/getOrderItem.js
@@ -28,7 +28,7 @@ getOrderItem.prototype.handleRoutes = function(router,connection){
             var keterangan = rows[0].keterangan;
             var status = rows[0].status;
 
-            var q = "select order_item.id as id_order_item,product.id as id_product,product_category.name,product.media,product.size,product.weight,order_item.quantity,order_item.price from `order` join `order_item` on order.id=order_item.order_id join `product` on order_item.product_id=product.id join `product_category` on product.category_id=product_category.id where order.no_bon = '"+no_bon+"'";
+            var q = "select order_item.id as id_order_item,product.id as id_product,product_category.id as id_product_category,product_category.name,product.media,product.size,product.weight,order_item.quantity,order_item.price from `order` join `order_item` on order.id=order_item.order_id join `product` on order_item.product_id=product.id join `product_category` on product.category_id=product_category.id where order.no_bon = '"+no_bon+"' order by product_category.id";
             connection.query(q,function(err,rows){
               if(err){
                 res.json({"message":"err.. error on selecting","error":"error"});
